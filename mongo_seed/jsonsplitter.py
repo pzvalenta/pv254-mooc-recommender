55065f06ee3c97f346774028757620e66568c8ef
Petr <justreverse@gmail.com>
2019-11-08 14:49:28 +0100

    adapt jsonsplitter to new data format & new dump

--- mongo_seed/jsonsplitter.py
+++ mongo_seed/jsonsplitter.py
@@ -1,26 +1,20 @@
 import json
 
-inputfiles = ['art-and-design_courses.json',
-'business_courses.json',
-'cs_courses.json',
-'data-science_courses.json',
-'education_courses.json',
-'engineering_courses.json',
-'health_courses.json',
-'humanities_courses.json',
-'maths_courses.json',
-'personal-development_courses.json',
-'programming-and-software-development_courses.json',
-'science_courses.json',
-'social-sciences_courses.json']
+inputfiles = ['courses.json']
 
 for inputfile in inputfiles:
     with open('courses/data/'+inputfile) as json_file:
-        data = json.load(json_file)
-        for key in data.keys():
-            newjson = data[key]
-            newjson['_id'] = newjson['id']
-            del newjson['id']
+        data = json.load(json_file)   
+       
+        data = data["courses"]
 
-            with open('courses/data/output/'+str(key)+'.json', 'w') as out_file:          
-                json.dump(newjson,out_file, indent = 4)
+        for key in data.keys():           
+            category = data[key]
+            
+            for key in category.keys():
+                newjson = category[key]
+                newjson['_id'] = newjson['id']
+                del newjson['id']
+
+                with open('courses/data/output/'+str(key)+'.json', 'w') as out_file:          
+                    json.dump(newjson,out_file, indent = 4)
